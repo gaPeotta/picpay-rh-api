cda5da9d0cfa93419acae5d3619485ab41a0b4b0
gaPeotta <gabriel.peotta@gmail.com>
2026-08-23 21:05:44 -0300

    Estrutura inicial do projeto Spring Boot

---
+++ src/test/java/com/picpay/rh/RhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
